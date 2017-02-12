7a5c5b6f696fdd1fba47e190dfdd8fcac18a6e29
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-02-12 12:54:44 +0100

    bug d'inscription pseudo existant corrigé

--- post_inscription.php
+++ post_inscription.php
@@ -1,9 +1,22 @@
 <?php 
+	$non_existant = true;
 	$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+	$liste_pseudo = $bdd->query('SELECT * FROM login');
 	if($_POST['futur_pseudo'] != '' AND $_POST['futur_password'] != '' AND $_POST['futur_password_deux'] == $_POST['futur_password']){
+
+		foreach ($liste_pseudo as $value) {
+			if($value['pseudo'] == ucfirst($_POST['futur_pseudo'])){
+				$non_existant = false;
+				break;
+			}
+		}
+
+		var_dump($non_existant, $value['pseudo'], $value['password']);
+		if($non_existant == true){
 		$inscription = $bdd->prepare('INSERT INTO login(pseudo, password) VALUES (:pseudo, :password) '); 
 		$inscription->execute(array('pseudo' => ucfirst($_POST['futur_pseudo']), 'password' => $_POST['futur_password']));
-		Header('Location: index.php');
+		}
+		//Header('Location: index.php');
 	}
 
 ?>
